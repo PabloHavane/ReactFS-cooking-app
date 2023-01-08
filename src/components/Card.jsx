94afef0c80250c311e37aa84b7e551a68db644c6
Pablo <oscar282004@gmail.com>
2023-01-08 19:57:16 +0100

    Petite modif dans note et suppr de "Category"

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,6 @@ const Card = ({ recipe }) => {
     <li className="card">
       <h3>{recipe.strMeal}</h3>
       <h4>{recipe.strArea}</h4>
-      <p>Category: {recipe.strCategory}</p>
       <img src={recipe.strMealThumb} alt={"Plat de " + recipe.strMeal} />
     </li>
   );
